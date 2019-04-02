8b23e53b149f9ad4bf4b4bae44ea78755496415f
Phainix <28295747+Phainix@users.noreply.github.com>
2019-04-02 15:03:10 +0100

    Hello Rest Cors

---
+++ HelloRestCors/src/main/java/hello/GreetingController.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@CrossOrigin(origins = "http://localhost:8000")
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(required = false, defaultValue = "World", value = "name") String name) {
+		System.out.println("======  in greeting  ======");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("/greeting-javaconfig")
+	public Greeting greetingWithJavaConfig(@RequestParam(required = false, defaultValue = "World") String name) {
+		System.out.println("======  in greeting  ======");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+}
